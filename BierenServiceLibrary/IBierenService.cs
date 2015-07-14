436a931a019bda8fefbf15186e25cd2de8f519e3
Caroline <caroline.hermans10@gmail.com>
2015-07-14 15:33:18 +0200

    c1

---
+++ BierenServiceLibrary/IBierenService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BierenServiceLibrary
+{
+    [ServiceContract(Namespace = "http://vdab.be/bierenservice")]
+    public interface IBierenService
+    {
+        [OperationContract]
+        int GetTotaalAantalBieren();
+        [OperationContract]
+        int GetAantalBierenTussenAlcohol(decimal van, decimal tot);
+        [OperationContract]
+        List<Bier> GetBierenMetWoord(string woord);
+    }
+}
